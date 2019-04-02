6f06c00843c35a2082a445b20a92ce276251bb42
mistersquiish <henryvuong@utexas.edu>
2019-04-02 17:44:49 -0500

    first commit

---
+++ ValetStorage/ValetStorage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ValetStorage
+//
+//  Created by Henry Vuong on 4/2/19.
+//  Copyright © 2019 Team7MIS374. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ValetStorage/ValetStorageTests/ValetStorageTests.swift
@@ -0,0 +1,36 @@
+//
+//  ValetStorageTests.swift
+//  ValetStorageTests
+//
+//  Created by Henry Vuong on 4/2/19.
+//  Copyright © 2019 Team7MIS374. All rights reserved.
+//
+
+import XCTest
+@testable import ValetStorage
+
+class ValetStorageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
