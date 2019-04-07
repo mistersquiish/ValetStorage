6de7aee87c1ab0a1b2381cd02052b477fb36f005
jj <jerry.sun@utexas.edu>
2019-04-06 21:48:47 -0500

    OnBoarding

--- ValetStorage/ValetStorage/ViewController.swift
+++ ValetStorage/ValetStorage/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let storyboard = UIStoryboard(name: "OnBoarding", bundle: nil)
+        if let walkthroughViewController = storyboard.instantiateViewController(withIdentifier: "walkthroughViewController")as? WalkThroughContentController{
+            present(walkthroughViewController, animated: true, completion: nil)
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
