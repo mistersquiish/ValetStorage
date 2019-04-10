fdd84f74ce1ef02fe5fd16a87986f21ff3942992
jj <jerry.sun@utexas.edu>
2019-04-10 10:34:25 -0500

    Fixed Merge

--- ValetStorage/ViewController/WalkThrough/WalkThroughContentController.swift
+++ ValetStorage/ViewController/WalkThrough/WalkThroughContentController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-class WalkThroughContentController: UIViewController {
+class WalkthroughContentViewController: UIViewController {
     
     // MARK: - Outlets
-    @IBOutlet var headinglabel: UILabel! {
-        didSet{
-            headinglabel.numberOfLines = 0
+    
+    @IBOutlet var headingLabel: UILabel! {
+        didSet {
+            headingLabel.numberOfLines = 0
         }
     }
+    
     @IBOutlet var subHeadingLabel: UILabel! {
         didSet {
             subHeadingLabel.numberOfLines = 0
@@ -31,26 +33,23 @@ class WalkThroughContentController: UIViewController {
     var subHeading = ""
     var imageFile = ""
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        headinglabel.text = heading
+        headingLabel.text = heading
         subHeadingLabel.text = subHeading
         contentImageView.image = UIImage(named: imageFile)
-
-        // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
